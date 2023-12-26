bb92fa7a835223ae2e027720cb3c42c18fc0af08
Lingyong Sun <lingyong.sun@gmail.com>
2023-12-26 13:00:30 +0100

    add istruzione-formazione

---
+++ pages/istruzione-formazione.vue
@@ -0,0 +1,16 @@
+<template>
+  <section id="istruzione_formazione">
+    <h1>{{ $t('istruzione.title') }}</h1>
+    <dl>
+      <template v-for="(istruzione, istruzioneKey) in $tm('istruzione.corsi')">
+        <dt>
+            {{ $t(`istruzione.corsi.${istruzioneKey}.periodo`) }}
+        </dt>
+        <dd>
+          <p v-html="$t(`istruzione.corsi.${istruzioneKey}.istituto`)"></p>
+          <p v-html="$t(`istruzione.corsi.${istruzioneKey}.corso`)"></p>
+        </dd>
+      </template>
+    </dl>
+  </section>
+</template>
