5128ebb5cf54e7bb81cf631803efd983c6c70a5b
Lingyong Sun <lingyong.sun@gmail.com>
2023-12-26 13:22:17 +0100

    add anagrafica

---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <section id="informazioni_personali">
+    <h1>{{ $t('anagrafica.title') }}</h1>
+    <dl>
+        <dt>
+            {{ $t('anagrafica.nominativo.title') }}
+        </dt>
+        <dd>
+            <p>
+                <span id="nome">{{ $t('anagrafica.nominativo.nome') }}</span> <span id="cognome">{{ $t('anagrafica.nominativo.cognome') }}</span>
+            </p>
+        </dd>
+        <!-- @if($withMail)
+        <dt>Email</dt>
+        <dd>{{ env('MAIL_PERSONAL') }}</dd>
+        @endif -->
+        <dt>
+            {{ $t('anagrafica.socials.title') }}
+        </dt>
+        <!-- <dd>
+            <dl id="contatti_social">
+                @foreach (json_decode(Storage::disk('data')->get('me/social-accounts.json')) as $social)
+                <dt>{{ $social->name }}</dt>
+                <dd>
+                    <a href="{{ $social->url }}" target="blank">{{ $social->username }}</a>
+                </dd>
+                @endforeach
+                <dt>
+                    Skype
+                </dt>
+                <dd class="last">
+                    lysz210
+                </dd>
+                <dt class="print_only">
+                    Curriculum online
+                </dt>
+                <dd class="print_only">
+                    <a href="{{Request::url()}}" target="blank">Curriculum vitae online</a>
+                </dd>
+            </dl>
+        </dd> -->
+    </dl>
+  </section>
+  <slot></slot>
+</template>
