88400231232f51a731b5299dc749081fae139a32
Lingyong Sun <lingyong.sun@gmail.com>
2023-12-26 12:31:28 +0100

    update

--- pages/esperienze-lavorative.vue
+++ pages/esperienze-lavorative.vue
@@ -2,21 +2,20 @@
   <section id="esperienze_professionali">
     <h1>{{ $t('lavoro.title') }}</h1>
     <dl>
-    <template v-for="(esperienza, esperienzaKey) in $tm('lavoro.esperienze')">
-      <dt>{{ $t(`lavoro.esperienze.${esperienzaKey}`) }}</dt>
-      <dd>
+      <template v-for="(esperienza, esperienzaKey) in $tm('lavoro.esperienze')">
+        <dt>{{ $t(`lavoro.esperienze.${esperienzaKey}`) }}</dt>
+        <dd>
           <h2 v-html="$t(`lavoro.esperienze.${esperienzaKey}.ruolo`)"></h2>
           <p v-html="$t(`lavoro.esperienze.${esperienzaKey}.azienda`)"></p>
-          <!-- <ul v-if="esperienza.attivita">
-              <li v-for="(_, attivitaKey) in $tm(`lavoro.esperienze.${esperienzaKey}.attivita`)">
-                {{ $t(`lavoro.esperienze.${esperienzaKey}.attivita.${attivitaKey}`) }}
-              </li>
-          </ul> -->
+          <ul v-if="esperienza.attivita">
+            <li v-for="(_, attivitaKey) in $tm(`lavoro.esperienze.${esperienzaKey}.attivita`)">
+              {{ $t(`lavoro.esperienze.${esperienzaKey}.attivita.${attivitaKey}`) }}
+            </li>
+          </ul>
           <p class="settore">
-              {{ $t(`lavoro.esperienze.${esperienzaKey}.settore`) }}
+            {{ $t(`lavoro.esperienze.${esperienzaKey}.settore`) }}
           </p>
-      </dd>
-    </template>
-    </dl>
-</section>
-</template>
+        </dd>
+      </template>
+    </dl>@endforeach
+  </section>
