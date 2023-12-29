20056d77a0c7592f15ed1f8da14e973ac4607635
Lingyong Sun <lingyong.sun@gmail.com>
2023-12-29 21:32:22 +0100

    install vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  build: {
+    transpile: [ 'vuetify' ]
+  },
   modules: [
-    '@nuxtjs/i18n'
+    '@nuxtjs/i18n',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', config => {
+        // @ts-expect-error
+        config.plugins?.push(vuetify({ autoImport: true }))
+      })
+    }
   ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls
+      }
+    }
+  },
   i18n: {
     lazy: true,
     langDir: 'lang',
