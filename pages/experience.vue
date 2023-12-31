0b785c19d2137c52824aa493ed5c7a05590867b2
Lingyong Sun <lingyong.sun@gmail.com>
2023-12-31 12:32:50 +0100

    vuetify workexperiences

--- pages/experience.vue
+++ pages/experience.vue
@@ -1,24 +1,39 @@
 <template>
-  <section id="esperienze_professionali">
-    <h1>{{ $t('lavoro.title') }}</h1>
-    <dl>
-      <template v-for="(esperienza, esperienzaKey) in $tm('lavoro.esperienze')">
-        <dt>{{ $t(`lavoro.esperienze.${esperienzaKey}.periodo`) }}</dt>
-        <dd>
-          <h2 v-html="$t(`lavoro.esperienze.${esperienzaKey}.ruolo`)"></h2>
-          <p v-html="$t(`lavoro.esperienze.${esperienzaKey}.azienda`)"></p>
-          <ul v-if="esperienza.attivita">
-            <li v-for="(_attivita, attivitaKey) in $tm(`lavoro.esperienze.${esperienzaKey}.attivita`)">
-              {{ $t(`lavoro.esperienze.${esperienzaKey}.attivita.${attivitaKey}`) }}
-            </li>
-          </ul>
-          <p class="settore">
-            {{ $t(`lavoro.esperienze.${esperienzaKey}.settore`) }}
-          </p>
-        </dd>
-      </template>
-    </dl>
-  </section>
+  <v-card id="esperienze_professionali">
+    <v-card-text>
+      <v-timeline
+        side="end"
+      >
+        <v-timeline-item dot-color="blue" v-for="(esperienza, esperienzaKey) in $tm('lavoro.esperienze')">
+          <template v-slot:opposite>
+            <v-card variant="flat">
+              <v-card-subtitle>
+                {{ $t(`lavoro.esperienze.${esperienzaKey}.periodo`) }}
+              </v-card-subtitle>
+            </v-card>
+          </template>
+          <v-card>
+            <v-card-title v-html="$t(`lavoro.esperienze.${esperienzaKey}.ruolo`)"></v-card-title>
+            <v-card-subtitle v-html="$t(`lavoro.esperienze.${esperienzaKey}.azienda`)"></v-card-subtitle>
+            <v-card-text>
+              <p class="settore">
+                {{ $t(`lavoro.esperienze.${esperienzaKey}.settore`) }}
+              </p>
+              <v-list v-if="esperienza.attivita"
+                density="compact"
+              >
+                <v-list-item
+                  v-for="(_attivita, attivitaKey) in $tm(`lavoro.esperienze.${esperienzaKey}.attivita`)"
+                  :subtitle="$t(`lavoro.esperienze.${esperienzaKey}.attivita.${attivitaKey}`)"
+                  >
+                </v-list-item>
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </v-timeline-item>
+      </v-timeline>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script setup>
