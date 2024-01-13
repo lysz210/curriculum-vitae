e7201c04e8479875b741397efc4077d519e4b68e
Lingyong Sun <lingyong.sun@gmail.com>
2024-01-13 12:21:55 +0100

    add fontawesome

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -4,7 +4,26 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
+import { aliases, fa } from 'vuetify/iconsets/fa-svg'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
 export default defineNuxtPlugin( app => {
-    const vuetify = createVuetify({})
+    app.vueApp.component('font-awesome-icon', FontAwesomeIcon)
+    library.add(fas)
+    library.add(far)
+    library.add(fab)
+    const vuetify = createVuetify({
+        icons: {
+            defaultSet: 'fa',
+            aliases,
+            sets: {
+                fa
+            }
+        }
+    })
     app.vueApp.use(vuetify)
 })
